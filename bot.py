d9df61703dd9048e9d2c83b3f18957327c58a25a
Karmaico <karmaico685@gmail.com>
2025-12-08 16:11:33 +0200

    Atualizando bot

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import yt_dlp
 # =========================
 # CONFIGURAÇÕES
 # =========================
-TELEGRAM_TOKEN = "8539746186:AAEbpwMA2UEMyOdLRUVrdkY2FYpQsfBGsPk"
+TELEGRAM_TOKEN ="8539746186:AAEbpwMA2UEMyOdLRUVrdkY2FYpQsfBGsPk"
 
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
+bot = telebot.TeleBot(8539746186:AAEbpwMA2UEMyOdLRUVrdkY2FYpQsfBGsPk)
 
 # =========================
 # /start
@@ -22,7 +22,8 @@ def start(message):
     markup.add(types.InlineKeyboardButton("🌐 Redes Sociais", callback_data="redes"))
 
     texto = (
-        "Olá! Eu posso fazer várias coisas:\n\n"
+        "Olá! Sou bot do Karmaico me chamo Togi🌖 Fushiguro.\n"
+        "Eu posso fazer várias coisas:\n\n"
         "Escolha uma opção abaixo 👇"
     )
     bot.send_message(message.chat.id, texto, reply_markup=markup)
@@ -43,17 +44,30 @@ def callback_buttons(call):
         bot.send_message(call.message.chat.id, "🌐 Siga minhas redes sociais 👇", reply_markup=markup)
 
 # =========================
-# /gerar - números brasileiros
+# /gerar - número BR + formatação nova
 # =========================
 def gerar_numero(message):
-    ddd = random.choice([11,12,13,14,15,16,17,18,19,
-                         21,22,24,27,28,31,32,33,34,35,37,38,
-                         41,42,43,44,45,46,47,48,49,51,53,54,55,
-                         61,62,64,63,65,66,67,68,69,71,73,74,75,77,79,
-                         81,82,83,84,85,86,87,88,89,91,92,93,94,95,96,97,98,99])
-    numero = f"{ddd} 9{random.randint(1000,9999)}-{random.randint(1000,9999)}"
+    ddd = random.choice([
+        11,12,13,14,15,16,17,18,19,
+        21,22,24,27,28,31,32,33,34,35,37,38,
+        41,42,43,44,45,46,47,48,49,
+        51,53,54,55,
+        61,62,63,64,65,66,67,68,69,
+        71,73,74,75,77,79,
+        81,82,83,84,85,86,87,88,89,
+        91,92,93,94,95,96,97,98,99
+    ])
 
-    bot.send_message(message.chat.id, f"📱 Número gerado:\n`{numero}`", parse_mode="Markdown")
+    parte1 = random.randint(1000, 9999)
+    parte2 = random.randint(1000, 9999)
+
+    numero = f"+55 {ddd} 9{parte1}-{parte2}"
+
+    bot.send_message(
+        message.chat.id,
+        f"📱 Número aleatório gerado:\n`{numero}`",
+        parse_mode="Markdown"
+    )
 
 # =========================
 # /baixar - baixar vídeo usando yt-dlp
